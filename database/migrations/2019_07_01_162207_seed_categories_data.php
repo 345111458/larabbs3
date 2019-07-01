8f405fe8e9831753619acd682b91d271ba886919
vagrant <vagrant@homestead>
2019-07-01 10:28:37 +0000

    填充用户和话题数据

--- database/migrations/2019_07_01_162207_seed_categories_data.php
+++ database/migrations/2019_07_01_162207_seed_categories_data.php
@@ -42,6 +42,6 @@ class SeedCategoriesData extends Migration
      */
     public function down()
     {
-        DB::table('categories')->truecate();
+        DB::table('categories')->truncate();
     }
 }
