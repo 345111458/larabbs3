d801867e9f87eb557b182767ec62364539b29f75
vagrant <vagrant@homestead>
2019-07-01 08:15:36 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,6 +11,13 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+
+	public function __construct(){
+
+		$this->middleware('auth',['except' => ['show']]);
+	}
+
+
     public function show(User $user){
     	
         return view('users.show', compact('user'));
@@ -18,13 +25,15 @@ class UsersController extends Controller
 
 
     public function edit(User $user){
+    	$this->authorize('update' , $user);
 
     	return view('users.edit' , compact('user'));
     }
 
 
     public function update(UserRequest $request , User $user , ImageUploadHandler $uploader){
-
+    	$this->authorize('update' , $user);
+    	
     	$data = $request->all();
     	if ($request->avatar) {
     		$result = $uploader->save($request->avatar , 'avatars' , Auth::id());
@@ -32,7 +41,7 @@ class UsersController extends Controller
     			$data['avatar'] = $result['path'];
     		}
     	}
-    	
+
     	$user->update($data);
     	return redirect()->route('users.show' , $user)->with('success' , '个人资料更新成功');
     }
