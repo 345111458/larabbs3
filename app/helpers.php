8493d41eb86b4dd23f60e3d814109267fffc904a
vagrant <vagrant@homestead>
2019-07-01 11:21:02 +0000

    话题排序

--- app/helpers.php
+++ app/helpers.php
@@ -14,7 +14,7 @@ function category_nav_active($category_id){
 	return active_class(if_route('categories.show') && if_route_param('category',$category_id));
 }
 
-// 分类
+// 分类 菜单按钮
 function category_menu(){
 
 	return Category::all();
