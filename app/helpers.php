c3441cb5d54e321fe7403eb38e7d51c3bb48c5cb
vagrant <vagrant@homestead>
2019-07-02 03:47:23 +0000

    用户可以创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -19,3 +19,11 @@ function category_menu(){
 
 	return Category::all();
 }
+
+
+
+function make_excerpt($value, $length = 200){
+	
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
